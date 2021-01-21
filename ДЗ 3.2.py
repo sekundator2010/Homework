10cae5413cbf5f9e846fb652bfe4b2d1b74cb06c
sekundator2010 <smaragdin000@gmail.com>
2021-01-21 21:02:28 +0700

    Create 3 homework

---
+++ ДЗ 3.2.py
@@ -0,0 +1,3 @@
+def intelligence(name, surname, year_birth, year_residence, email):
+    print(name, surname, year_birth, year_residence, email)
+intelligence(email= 'smaragdin000@gmail.com', surname= 'Levchenko',name= 'Mikhail', year_birth= 1991, year_residence= 2020)
---
+++ ДЗ 3.6.py
@@ -0,0 +1,5 @@
+def int_func(a):
+    print(i.capitalize())
+    print(i.title())
+i = input('Введите маленькие латинские буквы')
+int_func(i)
